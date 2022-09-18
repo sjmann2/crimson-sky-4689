02fd82c42d708d7d110cea5346bab1869571a510
Sid Mann <sjmann2@outlook.com>
2022-09-18 12:54:28 -0500

    test for total calories

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -1,11 +1,3 @@
-# Story 1 of 3
-# ```
-# As a visitor
-# When I visit a dish's show page
-# I see the dishs name and description
-# And I see a list of ingredients for that dish
-# And I see the chef's name.
-# ```
 require 'rails_helper'
 
 RSpec.describe 'the dish show page' do
@@ -28,5 +20,24 @@ RSpec.describe 'the dish show page' do
       expect(page).to have_content(chef.name)
       expect(page).to_not have_content(dish_2.name)
     end
+
+    it 'I see the total calorite count for that dish' do
+      chef = Chef.create!(name: 'Chef Boyardee')
+      dish_1 = Dish.create!(name: 'Ravioli', description: 'Cheese filled pasta', chef_id: chef.id)
+      dish_2 = Dish.create!(name: 'Spaghettios', description: 'Round noodles with sauce', chef_id: chef.id)
+      ingredient_1 = Ingredient.create!(name: 'Cheese', calories: 120)
+      ingredient_2 = Ingredient.create!(name: 'Pasta', calories: 150)
+      DishIngredient.create!(dish_id: dish_1.id, ingredient_id: ingredient_1.id)
+      DishIngredient.create!(dish_id: dish_1.id, ingredient_id: ingredient_2.id)
+      visit dish_path(dish_1)
+
+      expect(page).to have_content('Total Calories: 270')
+    end
   end
 end
+# Story 2 of 3
+# ```
+# As a visitor
+# When I visit a dish's show page
+# I see the total calorie count for that dish.
+# ```
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,21 @@ RSpec.describe Dish, type: :model do
     it {should have_many :dish_ingredients}
     it {should have_many(:ingredients).through(:dish_ingredients)}
   end
+
+  describe 'instance methods' do
+    describe 'total_calories' do
+      it 'calculates total calories for the dish' do
+        chef = Chef.create!(name: 'Chef Boyardee')
+        dish_1 = Dish.create!(name: 'Ravioli', description: 'Cheese filled pasta', chef_id: chef.id)
+        dish_2 = Dish.create!(name: 'Spaghettios', description: 'Round noodles with sauce', chef_id: chef.id)
+        ingredient_1 = Ingredient.create!(name: 'Cheese', calories: 120)
+        ingredient_2 = Ingredient.create!(name: 'Pasta', calories: 150)
+        DishIngredient.create!(dish_id: dish_1.id, ingredient_id: ingredient_1.id)
+        DishIngredient.create!(dish_id: dish_1.id, ingredient_id: ingredient_2.id)
+
+        expect(dish_1.total_calories).to eq(270)
+        expect(dish_2.total_calories).to eq(0)
+      end
+    end
+  end
 end
